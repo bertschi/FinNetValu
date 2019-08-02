c35d7c79051b9c3016ff59dfa6895a538087da08
Nils Bertschinger <bertschi@casino.fias.uni-frankfurt.de>
2019-08-03 00:55:54 +0200

    data grid and trying parallel execution

--- misc/correlo.jl
+++ misc/correlo.jl
@@ -2,11 +2,14 @@
 
 using DataFrames
 using DataFramesMeta
-using Gadfly
+## using Gadfly
 import NLsolve
-import LinearAlgebra
+using LinearAlgebra
 import ForwardDiff
+using Distributions
 using FinNetValu
+using ProgressMeter
+using Distributed
 
 ## First we define some helper functions
 
@@ -35,20 +38,20 @@ function netstat(net, a₀, θ, stat_fun; samples = 25000)
         x = fixvalue(net, a)
         equityview(net, x) .+ debtview(net, x)
     end
-    deff = NLsolve.nlsolve(a -> val(net, a) .- net.d, net.d).zero ## Find point where all firms just default
-    μ = NLsolve.nlsolve(z -> Aτ(a₀, θ, z) .- deff, zeros(numfirms(net))).zero ## Find corresponding unconstrained asset values
-    # q = MvNormal(μ, 3.0) ## Importance density ... does not seem to work very well?!?
-    q = MvNormal(numfirms(net), 1.)
-    w(z) = exp(logpdf(MvNormal(numfirms(net), 1.), z) - logpdf(q, z))
-    function tmp()
-        z = rand(q)
-        FinNetValu.Sample(z, w(z))
-    end
-    Z = [tmp() for _ = 1:samples]
-    expectation(z -> w(z) .* stat_fun(net, a₀, θ, z), Z), Z
-    # expectation(z -> stat_fun(net, a₀, θ, z),
-    #             MonteCarloSampler(MvNormal(numfirms(net), 1.)),
-    #             samples)
+    # deff = NLsolve.nlsolve(a -> val(net, a) .- net.d, net.d).zero ## Find point where all firms just default
+    # μ = NLsolve.nlsolve(z -> Aτ(a₀, θ, z) .- deff, zeros(numfirms(net))).zero ## Find corresponding unconstrained asset values
+    # # q = MvNormal(μ, 3.0) ## Importance density ... does not seem to work very well?!?
+    # q = MvNormal(numfirms(net), 1.)
+    # w(z) = exp(logpdf(MvNormal(numfirms(net), 1.), z) - logpdf(q, z))
+    # function tmp()
+    #     z = rand(q)
+    #     FinNetValu.Sample(z, w(z))
+    # end
+    # Z = [tmp() for _ = 1:samples]
+    # expectation(z -> w(z) .* stat_fun(net, a₀, θ, z), Z), Z
+    expectation(z -> stat_fun(net, a₀, θ, z),
+                MonteCarloSampler(MvNormal(numfirms(net), 1.)),
+                samples)
     
 end
 
@@ -61,10 +64,37 @@ function calcΣ(net, x, Δ, a₀, θ)
     Δˢ = equityview(net, Δ)
     σ = θ.σ .* ones(numfirms(net))
     a₀ = a₀ .* ones(numfirms(net))
-    L = cholesky(θ.Lᵨ).L
-    Lˢ = diagm(0 => (1 ./ s)) * Δˢ * diagm(0 => a₀) * diagm(0 => σ) * L
+    Lˢ = diagm(0 => (1 ./ s)) * Δˢ * diagm(0 => a₀) * diagm(0 => σ) * θ.Lᵨ
     Lˢ * Lˢ'
 end
 
+@everywhere function compute(a₀, σ₁, σ₂, md12, md21, ms12, ms21, ρ)
+    net = XOSModel([0 ms12; ms21 0], [0 md12; md21 0], I, [1.0, 1.0])
+    θ = BlackScholesParams(0.02, 1.5, [σ₁, σ₂], cholesky([1 ρ; ρ 1]).L)
+    x, Δ = netstat(net, a₀, θ, valΔ)
+    Σ = calcΣ(net, x, Δ, a₀, θ)
+    Σ[1,2] / √(Σ[1,1] * Σ[2,2])
+end
+
 function cordata()
+    df = reduce(DataFrames.crossjoin,
+                [DataFrame(a = 10 .^ LinRange(-3, 1, 5)),
+                 DataFrame(sigma1 = [0.4, 0.8]),
+                 DataFrame(sigma2 = [0.4, 0.8]),
+                 DataFrame(md12 = 0:0.4:0.8),
+                 DataFrame(md21 = 0:0.4:0.8),
+                 DataFrame(ms12 = 0),
+                 DataFrame(ms21 = 0),
+                 DataFrame(rho = -0.4:0.4:0.8)])
+    
+    # @byrow! df begin
+    #     @newcol cor::Vector{Float64}
+    #     :cor = compute(:a, :sigma1, :sigma2, :md12, :md21, :ms12, :ms21, :rho)
+    #     next!(p)
+    # end
+    # p = Progress(nrow(df))
+    # cor = progress_pmap(x -> compute(x...), zip(df.a, df.sigma1, df.sigma2, df.md12, df.md21, df.ms12, df.ms21, df.rho);
+    #                     progress = p)
+    cor = @with df pmap(compute, :a, :sigma1, :sigma2, :md12, :md21, :ms12, :ms21, :rho)
+    @transform(df, cor = cor)
 end
