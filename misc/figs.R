ef57789e728a7e4dd4ef946be819a797c947507a
Nils Bertschinger <bertschi@casino.fias.uni-frankfurt.de>
2019-12-18 18:52:01 +0100

    nicer labels

--- misc/figs.R
+++ misc/figs.R
@@ -5,18 +5,24 @@ library(latex2exp)
 
 read_csv("data/cordata.csv") %>%
     filter(md12 >= md21) %>%
+    mutate(md12 = map_chr(md12, ~ sprintf("$m^d_{12} = %.1f$", .x)),
+           md21 = map_chr(md21, ~ sprintf("$m^d_{21} = %.1f$", .x))) %>%
     ggplot(aes(a0, rhoS,
                color = factor(rho),
                linetype = factor(sigma))) +
     geom_line() +
     scale_color_colorblind() +
-    facet_grid(md12 ~ md21) +
+    facet_grid(md12 ~ md21,
+               labeller = function (labels, multi_line = TRUE) {
+                   label_parsed(map(labels, ~ TeX(.x)),
+                                multi_line)
+               }) +
     scale_x_log10() +
     scale_y_continuous(breaks = seq(-0.4, 0.8, by = 0.4)) +
     theme_tufte() +
     theme(text = element_text(size = 16),
           panel.grid.major.y = element_line(color = "grey80")) +
-    labs(x = TeX("$a_{1,2}$"),
+    labs(x = TeX("$a_1 = a_2$"),
          y = TeX("$\\rho_s$"),
          color = TeX("$\\rho_a$"),
          linetype = TeX("$\\sigma$"))
@@ -76,17 +82,25 @@ bound_df <-
     mutate(boundary = pmap(list(a1, a2, md12, md21),
                            function(a1, a2, md12, md21)
                                suzuki_boundaries(a1, a2, 0, 0, md12, md21, 1, 1))) %>%
-    unnest(boundary)
+    unnest(boundary) %>%
+    mutate(md12 = map_chr(md12, ~ sprintf("$m^d_{12} = %.1f$", .x)),
+           md21 = map_chr(md21, ~ sprintf("$m^d_{21} = %.1f$", .x)))
 
 read_csv("data/cordata2.csv") %>%
     ## mutate(d1 = 1, d2 = 1) %>%
     ## filter(a1 >= d1 - md12 * d2) %>%
     ## filter(a2 >= d2 - md21 * d1) %>%
+    mutate(md12 = map_chr(md12, ~ sprintf("$m^d_{12} = %.1f$", .x)),
+           md21 = map_chr(md21, ~ sprintf("$m^d_{21} = %.1f$", .x))) %>%
     ggplot() +
     geom_raster(aes(a1, a2, fill = rhoS)) +
     scale_x_log10() +
     scale_y_log10() +
-    facet_grid(md12 ~ md21) +
+    facet_grid(md12 ~ md21,
+               labeller = function (labels, multi_line = TRUE) {
+                   label_parsed(map(labels, ~ TeX(.x)),
+                                multi_line)
+               }) +
     scale_fill_viridis(option = "magma") +
     geom_line(aes(b1, b2,
                   group = type),
